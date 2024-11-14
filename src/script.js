c66d45211bc6d465ac0c842b541fc0b07bb062c6
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-11-14 12:26:45 -0500

    used elapsed time to shape moving particles

--- src/script.js
+++ src/script.js
@@ -118,6 +118,8 @@ for(let i = 0; i < baseGeometry.count; i++) {
 gpgpu.particlesVariable = gpgpu.computation.addVariable('uParticles', gpgpuParticleShader, baseParticlesTexure)
 gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [ gpgpu.particlesVariable ])
 
+// Uniforms
+gpgpu.particlesVariable.material.uniforms.uTime = new THREE.Uniform(0)
 
 // Init
 gpgpu.computation.init()
@@ -201,6 +203,7 @@ const tick = () =>
     controls.update()
 
     // GPGPU Update
+    gpgpu.particlesVariable.material.uniforms.uTime.value = elapsedTime
     gpgpu.computation.compute()
     particles.material.uniforms.uParticlesTexture.value = gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable).texture
 
--- src/shaders/gpgpu/particles.glsl
+++ src/shaders/gpgpu/particles.glsl
@@ -1,14 +1,17 @@
+uniform float uTime;
+
 #include ../includes/simplexNoise4d.glsl
 
 void main() {
+  float time = uTime * 0.2;
   vec2 uv = gl_FragCoord.xy / resolution.xy;
   vec4 particle = texture(uParticles, uv);
 
   // Flow field
   vec3 flowField = vec3(
-    simplexNoise4d(vec4(particle.xyz + 0.0, 0.0)),
-    simplexNoise4d(vec4(particle.xyz + 1.0, 0.0)),
-    simplexNoise4d(vec4(particle.xyz + 2.0, 0.0))
+    simplexNoise4d(vec4(particle.xyz + 0.0, time)),
+    simplexNoise4d(vec4(particle.xyz + 1.0, time)),
+    simplexNoise4d(vec4(particle.xyz + 2.0, time))
   );
 
   flowField = normalize(flowField);
